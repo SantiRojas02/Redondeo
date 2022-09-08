8409f591599135ff0763ee0a5b77c39e23015d0c
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-07 22:10:33 -0300

    Redondeo.vb

---
+++ Redondeo/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main(args As String())
+        Dim numero, Redondeo, defecto As Integer
+
+
+
+        Console.Write("Ingrese un numero real: ")
+        numero = Console.ReadLine()
+
+        Redondeo = Math.Round(numero)
+        defecto = numero
+
+
+        Console.WriteLine("Numero redondeado: " & Redondeo)
+        Console.WriteLine("Numero por defecto: " & defecto)
+
+
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
